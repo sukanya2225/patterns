181a879c0d63d0facf7f89246143f58b915796c9
sukanya2225 <123503115+sukanya2225@users.noreply.github.com>
2023-01-30 14:21:50 -0600

    Update Patterns.java

--- src/main/java/com/patterns/Patterns.java
+++ src/main/java/com/patterns/Patterns.java
@@ -1,2 +1,11 @@
-package com.patterns;public class Patterns {
+package com.patterns;
+public class Patterns {
+  public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+    }
 }
